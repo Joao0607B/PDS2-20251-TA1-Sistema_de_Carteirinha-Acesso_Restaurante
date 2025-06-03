50881d53b81ac87ec8684f5dd55dd76b32104c2b
Ana Laura Soares Mello <163796404+Anamello03@users.noreply.github.com>
2025-06-03 20:03:52 -0300

    Update Pessoa.hpp

--- Pessoa.hpp
+++ Pessoa.hpp
@@ -2,8 +2,22 @@
 #define PESSOA_H
 #include<iostream>
 #include<string>
+using namespace std;
 
 class Pessoa{
-    
+    private:
+    string nome;
+    int cpf;
+
+    public:
+    Pessoa(string _nome, int _cpf);
+
+    void setNome(string _nome);
+    void setCpf(int _cpf);
+
+    string getNome();
+    int getCpf();
+
+    virtual void printInfo();
 };
 #endif
