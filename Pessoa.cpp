b1e75bb1521e16ffea4b8229264d063b17adece9
Ana Laura Soares Mello <163796404+Anamello03@users.noreply.github.com>
2025-06-03 20:04:49 -0300

    Update Pessoa.cpp

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -1,1 +1,13 @@
+#include "Pessoa.hpp"
 
+Pessoa::Pessoa(string _nome, int _cpf): nome(_nome), cpf(_cpf){};
+
+void Pessoa::setNome(string _nome){this->nome = _nome;}
+void Pessoa::setCpf(int _cpf){this->cpf = _cpf;}
+
+string Pessoa::getNome(){return this -> nome;}
+int Pessoa::getCpf(){return this-> cpf;}
+
+void Pessoa::printInfo(){
+    cout<<"Cadastro no nome de" <<getNome()<< "realizado com sucesso"<<endl;
+}
