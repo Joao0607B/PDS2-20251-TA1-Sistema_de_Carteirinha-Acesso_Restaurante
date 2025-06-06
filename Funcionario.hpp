ae1b877765971bbcff4f561d24d1aa97f4a30153
Joao0607B <168564497+Joao0607B@users.noreply.github.com>
2025-06-06 15:57:16 -0300

    Create Funcionario.hpp
    add Funcionario.hpp

---
+++ Funcionario.hpp
@@ -0,0 +1,43 @@
+#ifndef FUNCIONÁRIO_HPP
+#define FUNCIONÁRIO_HPP
+
+#include <iostream>
+#include <string>
+#include "Pessoa.hpp" //classe pai
+#include "Cliente.hpp"
+#include "AdmFump.hpp"
+#include "Caixa.hpp"
+#include "Aluno.hpp"
+#include "Professor.hpp"
+#include "Visitante.hpp"
+
+
+class Funcionario: public Pessoa{
+
+    private:
+        std::string _nome;
+        std::string _departamento;
+        std::float _saldo;
+        // std:: string _ultimoAcesso;
+
+    public:
+        Funcionario(std::string nome, std::departamento departamento, std::float saldo);
+
+        std::string virtual getNome();
+        std::string virtual getDepartamento();
+        std::float virtual getSaldo();
+
+        void virtual setNome(std::string nome);
+        void virtual setDepartamento(std::string departamento);
+        void virtual setSaldo(std::float saldo);
+
+        void virtual printInfo(); //printa nome, departamento e saldo
+
+        //transferirsaldo() outra função talvez
+        //BloquearCartao() outra função talvez
+
+
+        //Alterar departamento, chama setDepartamento
+        //Alterar saldo, chama setSaldo
+}
+#endif 
