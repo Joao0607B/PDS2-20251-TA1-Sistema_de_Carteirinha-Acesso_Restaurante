1bad8094e1d196063e966ecf3ee07a1b15e80af9
MBrant29 <matheusmagalhaescb@gmail.com>
2025-06-10 12:47:45 -0300

    Create AdmFump.hpp

---
+++ AdmFump.hpp
@@ -0,0 +1,27 @@
+#ifndef ADM_FUMP_HPP
+#define ADM_FUMP_HPP
+
+#include <vector>
+#include <string>
+#include "Funcionario.hpp"
+#include "Aluno.hpp"
+
+/// \brief Representa o administrador FUMP que gerencia dados do RU
+class AdmFump : public Funcionario {
+public:
+	AdmFump(const std::string& nome, const std::string& cpf,
+	        const std::string& usuario, const std::string& senha);
+
+	void cadastrarAluno(const Aluno& aluno);
+	double acessarSaldoTotal() const;
+	int acessarNumeroClientes() const;
+	int acessarNumeroRefeicoesPorRestaurante(const std::string& restaurante) const;
+	int acessarNumeroAlunosPorNivel(int nivel) const;
+
+private:
+	std::vector<Aluno> m_alunos;
+	std::vector<std::string> m_restaurantes;
+	std::vector<int> m_refeicoesPorRestaurante;
+};
+
+#endif // ADM_FUMP_HPP
