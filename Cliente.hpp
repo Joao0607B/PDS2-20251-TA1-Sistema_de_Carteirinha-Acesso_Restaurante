77ef7def7987dd6338c8534bfc9893c990e24f20
roncali <64441534+roncali@users.noreply.github.com>
2025-06-15 19:46:32 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -1,4 +1,27 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
+#include <iostream>
+#include "Pessoa.hpp"
+using namespace std;
+
+class Cliente: public Pessoa{
+    private:
+        float m_saldo;
+        string m_ultimoAcesso;
+        bool isBloqueado = true;
+
+    public:
+        Cliente(string nome, int cpf): Pessoa(nome, cpf), m_saldo(0){};
+        float getSaldo();
+        string getUltimoAcesso();
+        float setSaldo();
+        void bloquearCartao();
+        void printInfo();
+
+    private:
+        float setSaldo();
+        void transferirCredito();
+};
+
 #endif
