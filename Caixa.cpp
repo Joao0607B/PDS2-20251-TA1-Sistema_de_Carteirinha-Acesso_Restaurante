9788fc6e3cd0924f277c6910cc776306a5ce2747
Ana Laura Soares Mello <163796404+Anamello03@users.noreply.github.com>
2025-06-15 13:50:48 -0300

    Update Caixa.cpp

--- Caixa.cpp
+++ Caixa.cpp
@@ -1,8 +1,11 @@
 #include "Caixa.hpp"
+#include <ctime>
+#include <fstream>
+#include <sstream>
 
 static float preco;
-map<int, bool> Caixa::clientes_que_ja_entraram;
-
+map<string, map<int, int>> Caixa::refeicoesClientes;
+string bancoDeDados = "banco_de_dados";
 Caixa::Caixa(string nome, string usuario, string senha): Funcionario(nome), _usuario(usuario), _senha(senha){};
 
 void Caixa::setNome(string nome){this->_nome = nome;}
@@ -38,7 +41,7 @@ float Caixa::acessarValor(Cliente cliente){
 
     if(nivel == 1){
         preco = 0;
-    }
+    }   
     else if(nivel == 2){
         preco = 1;
     }
@@ -51,12 +54,15 @@ float Caixa::acessarValor(Cliente cliente){
     else{
         preco = 5.60;
     }
+    return preco;
 }
-void Caixa::acessoCliente(Cliente cliente, float& valorPago){
+bool Caixa::acessoCliente(Cliente cliente, float& valorPago, const string& tipoRefeicao){
     int cpf = cliente.getCpf(); //o getCpf tem que ser herdado de cliente, que foi herdado de pessoa
     float tarifa = acessarValor(cliente);
+    string data = getDataAtual();
+    string chave = data + "_" + tipoRefeicao;
 
-    if (clientes_que_ja_entraram.find(cpf) != clientes_que_ja_entraram.end()) {
+    if (refeicoesClientes[chave].count(cpf)) {
         cout << "Acesso negado: o cliente " << cliente.getNome() << " já entrou hoje.\n";//getnome tmb terá que ser herdado
         return false;
     }
@@ -64,14 +70,178 @@ void Caixa::acessoCliente(Cliente cliente, float& valorPago){
         cout << "Acesso negado: valor insuficiente. Tarifa: R$" << tarifa << ", pago: R$" << valorPago << "\n";
         return false;
     }
-    clientes_que_ja_entraram[cpf] = true;
+    int nivel = cliente.getNivel();
+    refeicoesClientes[chave][cpf] = nivel; 
     cout << "Acesso liberado para " << cliente.getNome()<<endl;
+    armazenarRefeicoesporDia();
     return true;
 }
-void Caixa::resetarEntradas() {
-    clientes_que_ja_entraram.clear();
+void Caixa::resetarEntradas() { 
+    for (auto it = refeicoesClientes.begin(); it != refeicoesClientes.end(); ) {
+        if (it->first.find(data) == 0) {
+            it = refeicoesClientes.erase(it);
+        } else {
+            ++it;
+        }
+    }
+    armazenarRefeicoesporDia();//para usar depois de cada refeição
 }
 void Caixa::selecionarRU(string RU, int caixa){
     setRU(string RU);
     setCaixa(int caixa);
+}
+string Caixa::getDataAtual() const {//para pegar a data para colocar no banco de dados
+    time_t t = time(nullptr);
+    tm* now = localtime(&t);
+    char buf[11];
+    strftime(buf, sizeof(buf), "%Y-%m-%d", now);
+    return std::string(buf);
+}
+void Caixa::armazenarRefeicoesporDia() const {
+    ofstream arquivo(bancoDeDados);
+    if (!arquivo.is_open()) {
+        cerr << "Erro ao abrir o arquivo de banco de dados.\n";
+        return;
+    }
+    map<string, int> totalPorDia;// para mapear o total(almoco e janta)
+
+    for (const auto& par : refeicoesClientes) {
+    const string& chave = par.first;        // exemplo: "2025-06-15_almoco"
+    size_t pos = chave.find('_');           // encontra o '_' entre a data e o tipoRefeicao
+    string data = chave.substr(0, pos);     // extrai só a data: "2025-06-15"
+    int total = par.second.size();          // quantos clientes únicos comeram nessa refeição
+    totalPorDia[data] += total;             // soma almoço e janta no mesmo dia
+    }
+    for (const auto& dia : totalPorDia) {
+        arquivo << dia.first << ":total=" << dia.second << "\n";
+    }
+
+    arquivo.close();
+}
+void Caixa::armazenarRefeicoesServidasMes(const string& bancoDeDados) const {
+    map<std::string, int> totalPorMes;
+
+        for (const auto& par : refeicoesClientes) {
+        const string& chave = par.first; 
+        size_t posData = chave.find('_');
+        string data = chave.substr(0, posData); 
+        string mes = data.substr(0, 7);         
+
+        int total = par.second.size(); 
+        totalPorMes[mes] += total;
+    }
+
+    ofstream arquivo(bancoDeDados, ios::app); // abre em modo append
+    if (!arquivo.is_open()) {
+        cerr << "Erro ao abrir o arquivo: " << bancoDeDados << endl;
+        return;
+    }
+
+    arquivo << "=== Total de refeições por mês ===\n";
+    for (const auto& mes : totalPorMes) {
+        arquivo << mes.first << ":" << mes.second << "\n";
+    }
+
+    arquivo << "==================================\n\n";
+    arquivo.close();
+}
+void Caixa::armazenarRefeicoesServidasNivel(const string& bancoDeDados) const{
+    map<string, map<int, int>> totalPorMesPorNivel;
+
+    for (const auto& par : refeicoesClientes) {
+        const string& chave = par.first;              
+        size_t pos = chave.find('_');
+        string data = chave.substr(0, pos);           
+        string mes = data.substr(0, 7);                
+
+        const map<int, int>& mapaCpfNivel = par.second;
+        for (const auto& cliente : mapaCpfNivel) {
+            int nivel = cliente.second;
+            totalPorMesPorNivel[mes][nivel]++;
+        }
+    }
+
+    ofstream arquivo(nomeArquivo, ios::app);
+    if (!arquivo.is_open()) {
+        cerr << "Erro ao abrir o arquivo: " << nomeArquivo << "\n";
+        return;
+    }
+
+    arquivo << "=== Total por mês por nível ===\n";
+    for (const auto& mes : totalPorMesPorNivel) {
+        const string& nomeMes = mes.first;
+        for (const auto& nivel : mes.second) {
+            arquivo << nomeMes << ":nivel_" << nivel.first << ":" << nivel.second << "\n";
+        }
+    }
+    arquivo << "================================\n\n";
+    arquivo.close();
+}
+void Caixa::armazenarSaldoTotalDia(const string& bancoDeDados) const{
+    map<string, float> saldoPorDia;
+
+    for (const auto& par : refeicoesClientes) {
+        const string& chave = par.first;             
+        size_t pos = chave.find('_');
+        string data = chave.substr(0, pos);         
+
+        const auto& mapaCpfNivel = par.second;
+        for (const auto& entrada : mapaCpfNivel) {
+            int nivel = entrada.second;
+
+            Cliente clienteTemporario;
+            clienteTemporario.setNivel(nivel);            
+
+            float valor = acessarValor(clienteTemporario);
+            saldoPorDia[data] += valor;
+        }
+    }
+
+    ofstream arquivo(bancoDeDados, ios::app);
+    if (!arquivo.is_open()) {
+        cerr << "Erro ao abrir o arquivo: " << bancoDeDados << "\n";
+        return;
+    }
+
+    arquivo << "=== Saldo total por dia ===\n";
+    for (const auto& dia : saldoPorDia) {
+        arquivo << dia.first << ";" << dia.second << "\n";
+    }
+    arquivo << "===========================\n\n";
+    arquivo.close();
+}
+void Caixa::armazenarSaldoTotalMes(const string& bancoDeDados) const{
+    map<string, float> saldoPorMes;
+
+    
+    for (const auto& par : refeicoesClientes) {
+        const string& chave = par.first;             
+        size_t pos = chave.find('_');
+        string data = chave.substr(0, pos);         
+        string mes = data.substr(0, 7);              
+
+        const auto& mapaCpfNivel = par.second;
+        for (const auto& entrada : mapaCpfNivel) {
+            int nivel = entrada.second;
+
+            Cliente clienteTemporario;
+            clienteTemporario.setNivel(nivel);
+
+            float valor = acessarValor(clienteTemporario);
+            saldoPorMes[mes] += valor;
+        }
+    }
+
+    ofstream arquivo(bancoDeDados, ios::app);
+    if (!arquivo.is_open()) {
+        cerr << "Erro ao abrir o arquivo: " << bancoDeDados<< "\n";
+        return;
+    }
+
+    arquivo << "=== Saldo total por mês ===\n";
+    for (const auto& mes : saldoPorMes) {
+        arquivo << mes.first << ";" << mes.second << "\n";
+    }
+    arquivo << "===========================\n\n";
+    arquivo.close();
 }
