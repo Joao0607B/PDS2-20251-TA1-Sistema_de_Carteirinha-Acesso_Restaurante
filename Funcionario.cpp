cef2d9816e5aac20e0de0905547bae10db927ef1
Ana Laura Soares Mello <163796404+Anamello03@users.noreply.github.com>
2025-06-22 23:15:04 -0300

    Update Funcionario.cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -44,8 +44,25 @@ void Funcionario::trocarSenha(string& novaSenha, string& confirmarSenha){
     }
 }
 void Funcionario::liberarRefeicao(*Cliente cliente){
+    if(cliente->getBloqueado()){//Verifica por meio do método da Classe cliente (getBloqueado) que é um bool que retorna true se o cliente estiver bloqueado e não liberao acesso se estiver bloqueado.
+        cout<<"Cliente Bloqueado!Acesso negado."<<endl;
+        return;
+    }
+    //conferir como o ultimo acesso funciona com a Larissa
+    if(!cliente->getUltimoAcesso().empty()){//Essa função verifica se houve um ultimo acesso verificando se a string está vazia ou não, e se houve ultimo acesso, ou seja, a seja a string não está vazia, libera a Refeição
+        cout<<"Cliente já realizou a refeição desse turno"<<endl;
+    }
+    float valorRefeicao = cliente->getValorRefeicaoAtual();//cria um atributo local(float) que recebe o valor da refeição de cada cliente.
 
+    if (cliente->getSaldo() < valorRefeicao) {//Verifica se o saldo  atual do clinte é suficiente para realizar a refeição e se não for não libera o acesso.
+        cout << "Saldo insuficiente. Refeição não permitida." << endl;
+        return;
+    }
+    cliente->setSaldo(cliente->getSaldo() - valorRefeicao);//Aqui estamos atualizando o saldo, ou seja descontando do saldo o valor da refeição que está sendo liberada.
+    cliente->registrarAcesso();//Toda vez que a entrada do cliente for liberada, essa entrada será registrada para uma próxima verificação de acesso.
+
+    cout << "Refeição liberada" << endl;
 }
 Funcionario::~Funcionario(){
-    
+     cout << "Destruindo objeto Funcionario: " <<endl;
 }
