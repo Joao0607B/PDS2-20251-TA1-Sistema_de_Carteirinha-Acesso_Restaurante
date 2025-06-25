7dc7272da6f806d622e7a9e00e41ac4f40b2e1e0
MBrant29 <matheusmagalhaescb@gmail.com>
2025-06-25 13:26:52 -0300

    Update Professor.hpp

--- include/Professor.hpp
+++ include/Professor.hpp
@@ -1,45 +1,23 @@
 #ifndef PROFESSOR_HPP
 #define PROFESSOR_HPP
 
-#include <iostream>
+#include "Cliente.hpp"
 #include <string>
-#include "Pessoa.hpp" 
-#include "Cliente.hpp" //classe pai
-#include "AdmFump.hpp"
-#include "Caixa.hpp"
-#include "Aluno.hpp"
-#include "Professor.hpp"
-#include "Visitante.hpp"
 
-class Professor: public Cliente {
+/// \brief Classe que representa um professor, herda de Cliente
+class Professor : public Cliente {
+private:
+	std::string m_departamento;
 
-    private:
-        std::string _nome;
-        std::string _Matricula;
-        std::string _Curso;
-        std::float _saldo;
-        // std:: string _ultimoAcesso;
+public:
+	Professor(const std::string& nome, const std::string& cpf,
+	          const std::string& departamento);
 
-    public:
-        Professor(std::string nome, std::string Matricula, std::float saldo, std::string Curso);
+	/// \brief Retorna o departamento do professor
+	std::string getDepartamento() const;
 
-        std::string virtual getNome() override;  //herança
-        std::string virtual getMatricula() override; //herança
-        std::string virtual getCurso() override; //herança
-        std::float virtual getSaldo() override; //herança
+	/// \brief Imprime informações do professor
+	void printInfo() const override;
+};
 
-        void virtual setNome(std::string nome) override; //herança
-        void virtual setMatricula(std::string Matricula) override; //herança
-        void virtual setCurso(std::string Curso) override; //herança
-        void virtual setSaldo(std::float saldo) override; //herança
-
-        void virtual printInfo() override; //herança
-
-        //transferirsaldo() outra função talvez
-        //BloquearCartao() outra função talvez
-
-
-        //Alterar departamento, chama setDepartamento
-        //Alterar saldo, chama setSaldo
-}
-#endif 
+#endif
