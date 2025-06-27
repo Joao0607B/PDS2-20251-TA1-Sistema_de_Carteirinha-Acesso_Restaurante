89543cf0041bc2e2aa15c2cb657f853f773d57be
roncali <64441534+roncali@users.noreply.github.com>
2025-06-26 21:14:11 -0300

    Separei .cpp e .hpp, acrescentei ==, != e definirDataAtual

--- include/Data.hpp
+++ include/Data.hpp
@@ -1,105 +1,38 @@
 #ifndef DATA_H
 #define DATA_H
 
-#include "Pessoa.hpp"
 #include <string>
 #include <vector>
-using namespace std;
+#include <chrono>
+#include <ctime>
+#include <iostream>
 
-class Data{
-    private:
-        int _dia;
-        int _mes;
-        int _ano;
+class Data {
+private:
+    int _dia;
+    int _mes;
+    int _ano;
 
-        bool isAnoBissexto(int a) const {
-            return (a % 4 == 0 && a % 100 != 0) || (a % 400 == 0);
-        }
+    bool isAnoBissexto(int a) const;
+    bool validarData(int d, int m, int a) const;
 
-        //Verifica se uma data é possível. e.g. se o ano é positivo, se o mês é entre 1 e 12, etc.
-        bool validarData(int d, int m, int a) const {
-            if (a < 1) return false; 
-            if (m < 1 || m > 12) return false;
+public:
+    Data();
+    Data(int d, int m, int a);
 
-            // Vetor para armazenar o número de _dias em cada mês (ignorando o índice 0)
-            vector<int> _diasNoMes = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            if (isAnoBissexto(a)) {
-                _diasNoMes[2] = 29;
-            }
+    void setDia(int d);
+    void setMes(int m);
+    void setAno(int a);
 
-            if (d < 1 || d > _diasNoMes[m]) {
-                return false; 
-            }
-            return true;
-        }
+    int getDia() const;
+    int getMes() const;
+    int getAno() const;
 
-    public:
-        // Construtor
-        Data(int d = 5, int m = 10, int a = 2001) {
-            if (validarData(d, m, a)) {
-                _dia = d;
-                _mes = m;
-                _ano = a;
-            } else {
-                std::cout << "Data inserida inválida. Data armazenada: 05/10/2001." << std::endl;
-                _dia = 05;
-                _mes = 10;
-                _ano = 2001;
-            }
-        }
+    std::string toString() const;
+    void definirDataAtual();
 
-        //Setters e getters
-        void setDia(int d) {
-            if (validarData(d, _mes, _ano)) {
-                _dia = d;
-            } else {
-                std::cout << "Dia inválido. Data não alterada." << std::endl;
-            }
-        }
-
-        void setMes(int m) {
-            if (validarData(_dia, m, _ano)) {
-                _mes = m;
-            } else {
-                std::cout << "Mês inválido. Data não alterada." << std::endl;
-            }
-        }
-
-        void setAno(int a) {
-            if (validarData(_dia, _mes, a)) {
-                _ano = a;
-            } else {
-                std::cout << "Ano inválido. Data não alterada." << std::endl;
-            }
-        }
-
-        int getDia() const {
-            return _dia;
-        }
-
-        int getMes() const {
-            return _mes;
-        }
-
-        int getAno() const {
-            return _ano;
-        }
-
-        string toString(){
-            string d, m, a;
-            if (_dia<10)
-            {
-                d.append("0");
-                d.append(to_string(_dia));
-            }
-            if (_mes<10)
-            {
-                m.append("0");
-                m.append(to_string(_mes));
-            }
-            return d+m+a;
-            
-        }
+    bool operator==(const Data& outraData) const;
+    bool operator!=(const Data& outraData) const;
 };
 
 #endif
