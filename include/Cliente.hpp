34686de60ca05f9a3ba0f22123cf1316702547a9
roncali <64441534+roncali@users.noreply.github.com>
2025-06-28 15:22:46 -0300

    Update Cliente.hpp

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -8,13 +8,29 @@ using namespace std;
 class Cliente: public Pessoa{
     private:
         float _saldo;
-        bool isBloqueado;
-        float valorRefeicao;
-        Data ultimoAlmoco;
-        Data ultimoJantar;
+        bool _isBloqueado;
+        float _valorRefeicao;
+        Data _ultimoAlmoco;
+        Data _ultimoJantar;
 
     public:
-        Cliente(Pessoa(string nome, string cpf)); // inicia o saldo com 0, bloqueado como false, ultimoAlmoco e últimoJantar como string vazia
+        Cliente(string nome, string cpf); // inicia o saldo com 0, bloqueado como false, valor da refeição como R$17,50
+        void setSaldo(float valor);
+        float getSaldo();
+        void transferir(float valor, Cliente *outroCliente);
+        void depositar(float valor);
+        void bloquear();
+        void desbloquear();
+        void setUltimoAlmoco();
+        void setUltimoJantar();
+        Data getUltimoAlmoco();
+        Data getUltimoJantar();
+        void printInfo() override;
+        bool getIsBloqueado();
+        float getValorRefeicao();
+        void setValorRefeicao(float valor);
+        void registrarAcesso (char tipoDeRefeicao);
+
 };
 
 #endif
