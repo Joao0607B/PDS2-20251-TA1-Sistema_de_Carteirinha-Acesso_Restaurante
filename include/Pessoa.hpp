977dbba5272e7ef2f3bf2222b6edaa8af05d099f
roncali <64441534+roncali@users.noreply.github.com>
2025-06-29 10:03:42 -0300

    Doxygen Pessoa.hpp

--- include/Pessoa.hpp
+++ include/Pessoa.hpp
@@ -1,26 +1,63 @@
+/**
+ * @file Pessoa.hpp
+ * @brief Definição da classe base Pessoa, que representa um indivíduo no sistema.
+ */
+
 #ifndef PESSOA_H
 #define PESSOA_H
-#include<iostream>
-#include<string>
-using namespace std;
 
-class Pessoa{
-//Pessoa é a superclasse geral,logo se apresenta no topo do diagrama de herança de classes.
-    private:
-//Pessoa possui dois argumentos/atributos privados(_nome e _cpf)
-    string _nome; 
-    string _cpf;
+#include <iostream>
+#include <string>
 
-    public:
-    Pessoa(string nome, string cpf);//Construtor que recebe os arqumentos nome e cpf e é criado toda vez que um objeto é criado.
+/**
+ * @class Pessoa
+ * @brief Superclasse que modela as características comuns a todos os indivíduos.
+ * * Armazena informações básicas como nome e CPF. Serve como classe base para
+ * outras classes mais específicas como Cliente e Funcionario.
+ */
+class Pessoa {
+private:
+    std::string _nome; ///< Armazena o nome completo da pessoa.
+    std::string _cpf;  ///< Armazena o Cadastro de Pessoas Físicas (CPF).
 
-    void setNome(string nome);//Aqui temos o Set para permitir modificar o argumento privado _nome.
-    void setCpf(string cpf);//Aqui temos o Set para permitir modificar o argumento privado _cpf.
+public:
+    /**
+     * @brief Construtor da classe Pessoa.
+     * @param nome O nome da pessoa.
+     * @param cpf O CPF da pessoa.
+     */
+    Pessoa(std::string nome, std::string cpf);
 
-    string getNome();//Aqui temos o Get para permitir acessar o argumento privado _nome.
-    string getCpf();//Aqui temos o Get para permitir acessar o argumento privado _cpf.
+    /**
+     * @brief Define ou atualiza o nome da pessoa.
+     * @param nome O novo nome a ser atribuído.
+     */
+    void setNome(std::string nome);
 
-    virtual void printInfo();//O método printInfo() virtual que poderá ser alterado nas subclasse. Sua função na classe Pessoa é printar as informações pessoais do usuário, que nessa classe é o nome e o cpf.
-    ~Pessoa();//O destrutor que será chamado todo final de função, ou final do programa, ou quando o delete for chamado e é responsável por limpar os objetos criados.
+    /**
+     * @brief Define ou atualiza o CPF da pessoa.
+     * @param cpf O novo CPF a ser atribuído.
+     */
+    void setCpf(std::string cpf);
+
+    /**
+     * @brief Obtém o nome da pessoa.
+     * @return Uma string contendo o nome da pessoa.
+     */
+    std::string getNome();
+
+    /**
+     * @brief Obtém o CPF da pessoa.
+     * @return Uma string contendo o CPF da pessoa.
+     */
+    std::string getCpf();
+
+    /**
+     * @brief Imprime as informações da pessoa no console.
+     * * Este método é virtual e pode ser sobrescrito pelas classes derivadas
+     * para incluir informações adicionais.
+     */
+    virtual void printInfo();
 };
-#endif
+
+#endif // PESSOA_H
