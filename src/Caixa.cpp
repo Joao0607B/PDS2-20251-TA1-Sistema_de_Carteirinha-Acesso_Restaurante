7f94fb9b11c6c75788d79873c6fc1d87acb11301
roncali <64441534+roncali@users.noreply.github.com>
2025-06-29 16:52:35 -0300

    Doxygen Caixa.cpp

--- src/Caixa.cpp
+++ src/Caixa.cpp
@@ -1,8 +1,26 @@
-#include "Caixa.hpp"
+/**
+ * @file Caixa.cpp
+ * @brief Implementação dos métodos da classe Caixa.
+ * @see Caixa.hpp
+ */
+#include "../include/Caixa.hpp"
 
-Caixa::Caixa(string nome, string cpf, string usuario, string senha, string restaurante, int caixa): Funcionario(nome, cpf, usuario, senha), _restaurante(""), _caixa(0){}
+// Utiliza a lista de inicialização para atribuir os valores passados aos membros da classe.
+Caixa::Caixa(std::string nome, std::string cpf, std::string usuario, std::string senha, std::string restaurante, int caixa)
+    : Funcionario(nome, cpf, usuario, senha), _restaurante(restaurante), _caixa(caixa) {}
 
-void Caixa::setCaixa(int caixa){this->_caixa = caixa;}
-void Caixa::setRestaurante(string restaurante){this->_restaurante = restaurante;}
-int Caixa::getCaixa(){return this -> _caixa;}
-string Caixa::getRestaurante(){return this-> _restaurante;}
+void Caixa::setCaixa(int caixa) {
+    this->_caixa = caixa;
+}
+
+void Caixa::setRestaurante(std::string restaurante) {
+    this->_restaurante = restaurante;
+}
+
+int Caixa::getCaixa() {
+    return this->_caixa;
+}
+
+std::string Caixa::getRestaurante() {
+    return this->_restaurante;
+}
